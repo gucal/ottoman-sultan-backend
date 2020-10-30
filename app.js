300d88fa4ca9202ec7498c4f542f68167de24213
Gucal <gucalaliriza@outlook.com>
2020-10-30 21:36:34 +0300

    express urlencoded true

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
